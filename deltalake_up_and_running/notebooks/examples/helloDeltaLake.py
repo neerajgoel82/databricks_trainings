c174bb1c38d9908148ac1be0a1771ffea604077a
neerajgoel82 <neerajgoel82>
2023-03-23 21:56:56 +0000

    Updating the notebook comments

--- deltalake_up_and_running/notebooks/examples/helloDeltaLake.py
+++ deltalake_up_and_running/notebooks/examples/helloDeltaLake.py
@@ -1,19 +1,8 @@
 # Databricks notebook source
-# MAGIC %md-sandbox
-# MAGIC 
-# MAGIC <div style="text-align: center; line-height: 0; padding-top: 9px;">
-# MAGIC   <img src="https://databricks.com/wp-content/uploads/2018/03/db-academy-rgb-1200px.png" alt="Databricks Learning" style="width: 600px">
-# MAGIC </div>
-
-# COMMAND ----------
-
 # MAGIC %md # Delta Lake Lab
 # MAGIC ##### Tasks
-# MAGIC 1. Write sales data to Delta
-# MAGIC 1. Modify sales data to show item count instead of item array
-# MAGIC 1. Rewrite sales data to same Delta path
+# MAGIC 1. Write numbers data to Delta
 # MAGIC 1. Create table and view version history
-# MAGIC 1. Time travel to read previous version
 
 # COMMAND ----------
 
@@ -54,7 +43,7 @@ display(spark.sql("DESCRIBE HISTORY numbers_delta"))
 
 # COMMAND ----------
 
-# MAGIC %md **4.1: CHECK YOUR WORK**
+# MAGIC %md **2.1: CHECK YOUR WORK**
 
 # COMMAND ----------
 
@@ -71,11 +60,3 @@ print("All test pass")
 # COMMAND ----------
 
 DA.cleanup()
-
-# COMMAND ----------
-
-# MAGIC %md-sandbox
-# MAGIC &copy; 2023 Databricks, Inc. All rights reserved.<br/>
-# MAGIC Apache, Apache Spark, Spark and the Spark logo are trademarks of the <a href="https://www.apache.org/">Apache Software Foundation</a>.<br/>
-# MAGIC <br/>
-# MAGIC <a href="https://databricks.com/privacy-policy">Privacy Policy</a> | <a href="https://databricks.com/terms-of-use">Terms of Use</a> | <a href="https://help.databricks.com/">Support</a>
